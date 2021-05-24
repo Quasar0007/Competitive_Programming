98780ee917b8936b0aeb0ed698de750d9e3aaf33
Quasar0007 <me2191079@iitd.ac.in>
2021-05-25 00:17:45 +0530

    Competitive_Programming

---
+++ Greedy_Florist.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the getMinimumCost function below.
+def getMinimumCost(k, c):
+    c.sort(reverse = True)
+    cost=sum(c[0:k])
+    i=0
+    j=1
+    while i<len(c)-k :
+        cost+=(j+1)*sum(c[(k+i):k*(j+1)])
+        i+=k
+        j+=1
+    return cost
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    minimumCost = getMinimumCost(k, c)
+
+    fptr.write(str(minimumCost) + '\n')
+
+    fptr.close()
